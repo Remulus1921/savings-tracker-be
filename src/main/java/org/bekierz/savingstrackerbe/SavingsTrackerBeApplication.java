d6ad6934d55fd8930628c0881df6abd2cc8eca5e
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2025-01-02 11:24:38 +0100

    Initial commit

---
+++ src/main/java/org/bekierz/savingstrackerbe/SavingsTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package org.bekierz.savingstrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bekierz/savingstrackerbe/SavingsTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bekierz.savingstrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
